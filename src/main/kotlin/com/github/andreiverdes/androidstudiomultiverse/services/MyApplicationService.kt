df6c22a3c35f0a734e7d54e7f8e6f7a6fac8400f
GitHub Action <action@github.com>
2022-03-28 10:00:31 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/andreiverdes/androidstudiomultiverse/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.andreiverdes.androidstudiomultiverse.services
+
+import com.github.andreiverdes.androidstudiomultiverse.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/test/kotlin/com/github/andreiverdes/androidstudiomultiverse/MyPluginTest.kt
+++ src/test/kotlin/com/github/andreiverdes/androidstudiomultiverse/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.andreiverdes.androidstudiomultiverse
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
